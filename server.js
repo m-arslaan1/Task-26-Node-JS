99f53405b7df5e0e86d95e37fb487ee0122e735e
m-arslaan1 <mohd.arslaan49@gmail.com>
2024-12-20 18:09:09 +0530

    second commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-const port = 3000;
+const port = process.env.port;
 
 const server = http.createServer((req, res) => {
   let filePath = "";
